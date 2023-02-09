e93e0dd192e1dbcbd67f110918e33d4c662ab816
aniketj2002 <aniketjindal2002@gmail.com>
2023-02-09 17:11:54 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+      }
+    },
+  },
+  plugins: [],
+};
