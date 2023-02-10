457530f0446ed8a2e4dc69030e99bc298fbbf5f0
Aman Gupta <bhaiyakaku18@gmail.com>
2023-02-10 14:09:46 +0530

    updated navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
         mullish : ["Mulish ", "sans-serif"] , 
       },
       colors:{
+        bgBlue : "#bcdaf7" , 
+        bgBlueShadow : "#e4f0fe" , 
+        btnBlue : "#030448" , 
+        txtGrey : "#9b9a9b" , 
+        textBlue : "#1d8cf4" , 
         lightBlue: "#2585E2",
         lightBlue300: "#4b94ed",
         lightBlue500: "#0b72e7",
