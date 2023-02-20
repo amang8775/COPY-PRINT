a2141ba18d421d3ed3896e39f38edd137a2a39fd
aniketj2002 <aniketjindal2002@gmail.com>
2023-02-21 00:25:20 +0530

    herosection animation & favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,22 @@ module.exports = {
         lightBlue: "#2585E2",
         lightBlue300: "#4b94ed",
         lightBlue500: "#0b72e7",
-      }
-      
+      },
+      keyframes: {
+        pencil: {
+          '0%, 100%': { transform: 'translatex(-5%) translatey(5%)' },
+          '50%': { transform: 'translatex(5%) translatey(-5%)' },
+        },
+        hero: {
+          '0%, 100%': { transform: 'translatey(1%)' },
+          '50%': { transform: 'translatey(-1%)' },
+        }
+      },
+      animation: {
+        pencil: 'pencil 2s ease-in-out infinite',
+        hero: 'hero 5s ease-in-out infinite',
+        spin:'spin 3s ease-in-out infinite alternate'
+        }
     },
   },
   plugins: [],
